cedc4025a35a2ebaec7abc0380fb15c7595e50d9
Kouhei Nakata <koheinakata0508@gmail.com>
2016-10-15 04:15:58 +0900

    「今日渋谷でランチしよ」をheaderに追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,13 +33,18 @@ class TweetsController < ApplicationController
   def update
     tweet = Tweet.find(params[:id])
     if tweet.user_id == current_user.id
-      tweet.update(tweet_params)
+      tweet.update(edit_params)
     end
   end
 
   private
   def tweet_params
     params.require(:tweet).permit(:image, :text, :folderimage)
+  end
+
+  private
+  def edit_params
+    params.permit(:image, :text, :folderimage)
   end
 
   def move_to_index
